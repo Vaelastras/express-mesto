7490d23ae68c648e31981cb5eb7f0c9bc46cfea2
Alexey Belov <vaelastras@gmail.com>
2020-10-27 10:21:07 +0300

    bugfix 1st iteration

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const routes = require('./routes/index');
 
 const app = express();
+const { PORT = 3000 } = process.env;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -12,18 +14,13 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
-const { PORT = 3000 } = process.env;
-const path = require('path');
-const routes = require('./routes/index');
-
-app.use(express.static(path.join(`${__dirname}/public`)));
-app.use('/', routes);
-
 app.use((req, res, next) => {
   req.user = {
     _id: '5f953eed59efad2ed0ef0ee1', // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
   next();
 });
+
+app.use('/', routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- controllers/cards.js
+++ controllers/cards.js
@@ -1,3 +1,13 @@
+// При создании карточки сервис возвращает ошибку
+// со статус-кодом 500. Это связано с тем что мидлвару где вы проставляете пользователя
+// вы подключаете после маршрута пользователей.
+// После внесения исправления создание карточки начинает падать на ошибке валидации.
+// Включение отладочного логгирования поможет вам разобраться в причинах :)
+
+// Денис, я перенес мидлвару, но ошибка валидации воспроизводится
+// только если я указываю слишком короткое имя - в остальном все ок.
+// Возможно мы говорим о разных вещах? Пожалуйста проверьте еще раз, все должно работать корректно
+
 const Card = require('../models/card');
 
 const getAllCards = (req, res) => {
@@ -19,16 +29,17 @@ const createCard = (req, res) => {
 };
 
 const cardDeleteById = (req, res) => {
-  Card.findByIdAndRemove(req.params.id)
+  console.log(req.params);
+  Card.findByIdAndRemove(req.params.cardId)
     .then((card) => {
-      if (!card) {
+      if (card === null || undefined) {
         return res.status(404).send({ message: 'Oops, that card-ID doesn\'t exist' });
       }
       return res.send((card));
     })
     .catch((err) => {
       console.log(err);
-      if (err.name === 'ValidationError') {
+      if (err.name === 'CastError') {
         return res.status(400).send({ message: 'Validation error. Please type a right data!' });
       }
       return res.status(500).send({ message: `Internal Server error: ${err}` });
